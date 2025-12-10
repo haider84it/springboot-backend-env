d5a57985cd9f0f5a0c954ed348cfa8ccacd94bba
Hazim Haider <haider84it@gmail.com>
2025-12-10 13:29:23 +0100

    Add AttributeOverrides for Seite1 to expose anlagenbezeichnung for list view

--- src/main/java/com/example/pvbackend/model/Wartungsprotokoll.java
+++ src/main/java/com/example/pvbackend/model/Wartungsprotokoll.java
@@ -23,6 +23,14 @@ public class Wartungsprotokoll {
     // Seite 1
     // ---------------------------
     @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "anlagenbezeichnung", column = @Column(name = "anlagenbezeichnung")),
+            @AttributeOverride(name = "auftraggeber", column = @Column(name = "auftraggeber")),
+            @AttributeOverride(name = "vorgang", column = @Column(name = "vorgang")),
+            @AttributeOverride(name = "wartungspaket", column = @Column(name = "wartungspaket")),
+            @AttributeOverride(name = "dcMessungen", column = @Column(name = "dc_messungen")),
+            @AttributeOverride(name = "acMessungen", column = @Column(name = "ac_messungen"))
+    })
     private WartungsprotokollSeite1 seite1 = new WartungsprotokollSeite1();
 
 
