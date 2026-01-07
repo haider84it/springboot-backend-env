54cc97738bae0144ae96c43f21ff4d47fea0529c
Hazim Haider <haider84it@gmail.com>
2026-01-07 16:59:56 +0100

    - Prevent empty Seite 2,3 from being rendered in PDF

--- src/main/java/com/example/pvbackend/model/WartungsprotokollSeite2.java
+++ src/main/java/com/example/pvbackend/model/WartungsprotokollSeite2.java
@@ -13,7 +13,7 @@ import java.util.List;
 @Embeddable
 public class WartungsprotokollSeite2 {
 
-    // Zugangsschlüssel
+    // - Zugangsschlüssel
     private Boolean zugangsschluesselVorhanden;
     private String zugangsschluesselAnmerkung;
 
