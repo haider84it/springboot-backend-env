55f294a86e0a482e22110c1cbaca67483990e602
Hazim Haider <haider84it@gmail.com>
2026-01-27 12:10:44 +0100

    fix angabenZumDach save/update handling

--- src/main/java/com/example/pvbackend/controller/AngabenZumDachController.java
+++ src/main/java/com/example/pvbackend/controller/AngabenZumDachController.java
@@ -1,15 +1,12 @@
 package com.example.pvbackend.controller;
 
 
-
 import com.example.pvbackend.model.AngabenZumDachAnlage;
-import com.example.pvbackend.model.PhotovoltaikAnlage;
 import com.example.pvbackend.service.AngabenZumDachService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -39,36 +36,34 @@ public class AngabenZumDachController {
     }
 
     @PostMapping
-    public AngabenZumDachAnlage createAngabenZumDach(@RequestBody Map<String, Object> body) {
+    public AngabenZumDachAnlage createAngabenZumDach(
+            @RequestBody AngabenZumDachAnlage dach) {
 
-        List<String> dacharten = (List<String>) body.get("dacharten");
-        Map anlageMap = (Map) body.get("anlage");
-        Long anlageId = Long.valueOf(anlageMap.get("id").toString());
-
-        AngabenZumDachAnlage dach = new AngabenZumDachAnlage();
-        dach.setAnlage(new PhotovoltaikAnlage(anlageId));
-
-        dach.setSatteldach(dacharten.contains("Satteldach"));
-        dach.setFlachdach(dacharten.contains("Flachdach"));
-        dach.setPultdach(dacharten.contains("Pultdach"));
-        dach.setSheddach(dacharten.contains("Sheddach"));
-        dach.setAndere(dacharten.contains("andere"));
+        if (dach.getAnlage() != null) {
+            dach.getAnlage().setAngabenZumDachAnlage(dach);
+        }
 
         return angabenZumDachService.saveAngabenZumDach(dach);
     }
 
     @PutMapping("/{id}")
-    public AngabenZumDachAnlage updateAngabenZumDach(@PathVariable Long id, @RequestBody AngabenZumDachAnlage updateAngabenZumDach) {
-        updateAngabenZumDach.setId(id);
-        return angabenZumDachService.saveAngabenZumDach(updateAngabenZumDach);
-    }
+    public AngabenZumDachAnlage updateAngabenZumDach(
+            @PathVariable Long id,
+            @RequestBody AngabenZumDachAnlage dach) {
 
+        dach.setId(id);
+
+        if (dach.getAnlage() != null) {
+            dach.getAnlage().setAngabenZumDachAnlage(dach);
+        }
+
+        return angabenZumDachService.saveAngabenZumDach(dach);
+    }
 
     @DeleteMapping("/{id}")
     public void deleteAngabenZumDach(@PathVariable Long id) {
         angabenZumDachService.deleteAngabenZumDach(id);
     }
-
 
 
 }
